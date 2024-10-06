90e98f5a6c156bde34992fd8104beba34b9c09aa
pwnyy <hubertytom@hotmail.com>
2024-10-07 00:31:14 +0200

    Update code.cs

--- Get Color from Input/code.cs
+++ Get Color from Input/code.cs
@@ -56,6 +56,17 @@ public class CPHInline
 					b = b > 255 ? 255 : b < 0 ? 0 : b;
 					foundColor = Color.FromArgb(a, r, g, b);
 					break;
+				case "rgba":
+					r = inputs.TryGetValue("input1",out rString) ? int.TryParse(rString,out r)? r :0: 0;
+					g = inputs.TryGetValue("input2",out gString) ? int.TryParse(gString,out g)? g :0: 0;
+					b = inputs.TryGetValue("input3",out bString) ? int.TryParse(bString,out b)? b :0: 0;
+					a = inputs.TryGetValue("input4",out aString) ? int.TryParse(aString,out a)? a :0: 0;
+					r = r > 255 ? 255 : r < 0 ? 0 : r;
+					g = g > 255 ? 255 : g < 0 ? 0 : g;
+					b = b > 255 ? 255 : b < 0 ? 0 : b;
+					a = a > 255 ? 255 : a < 0 ? 0 : a;
+					foundColor = Color.FromArgb(a, r, g, b);
+					break;
 				default:
 					string nameInput = "";
 					foreach(KeyValuePair<string,string> word in inputs)
